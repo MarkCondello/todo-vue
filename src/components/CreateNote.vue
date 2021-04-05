7d0b727f9486e094f04cecb19f0c945394786acf
Mark Condello <mark@dcode.com.au>
2021-04-05 15:57:34 +1000

    Modifed layout for create note

--- src/components/CreateNote.vue
+++ src/components/CreateNote.vue
@@ -1,61 +1,85 @@
 <template>
-    <m-card outlined>
-        <template slot="actionButtons" v-if="editOptions">
-             <input type="text" name="title" v-model="title" placeholder="title"/>
-        </template>
-        <template slot="actionButtons" v-else>
-            <p @click="editOptions=true">Take a note...</p>
-        </template>
-
-        <template slot="actionIcons" v-if="editOptions">
-             <m-icon-button icon="push_pin" title="Pin note">
-            </m-icon-button> 
-        </template>
-        <template slot="actionIcons" v-else>
-            <m-icon-button icon="check_box" title="Check list">
-            </m-icon-button>
-            <m-icon-button icon="brush" title="Add Sketch">
-            </m-icon-button>
-            <m-icon-button icon="photo" title="Add Photo">
-            </m-icon-button>
-        </template>
-
- 
-            <m-layout-grid v-if="editOptions">
-             
-                <m-layout-grid-inner>
-                    <m-layout-grid-cell>
-                        <m-icon-button icon="notification_important" title="Add reminder">
-                        </m-icon-button>
-                        <m-icon-button icon="person_add_alt_1" title="Add user">
-                        </m-icon-button>
-                        <m-icon-button icon="palette" title="Colour palette">
-                        </m-icon-button>
-                        <m-icon-button icon="image" title="Add images">
-                        </m-icon-button>
-                        <m-icon-button icon="archive" title="Add to archive">
-                        </m-icon-button>
-                        <!-- Need Ellipsis -->
-                    </m-layout-grid-cell>
-                    <m-layout-grid-cell>
-                        <m-button>Close</m-button>
-                    </m-layout-grid-cell>
-                </m-layout-grid-inner>
+    <m-card>
+        <m-card-primary-action>
+            <m-layout-grid style="width:100%;">
+                <template v-if="editOptions">
+                    <m-layout-grid-inner>
+                        <m-layout-grid-cell :span="8">
+                            <input type="text" name="title" v-model="title" placeholder="Title"/>
+                        </m-layout-grid-cell >
+                        <m-layout-grid-cell :span="4" class="mdc-layout-grid--align-right">
+                            <m-icon-button icon="push_pin" title="Pin note">
+                            </m-icon-button> 
+                        </m-layout-grid-cell>
+                    </m-layout-grid-inner>
+                    <m-layout-grid-inner>
+                        <m-layout-grid-cell :span="12">
+                            <input ref="taskInput" class="smaller" type="text" name="task" v-model="task" placeholder="Take a note..."/>
+                        </m-layout-grid-cell>
+                    </m-layout-grid-inner>
+                </template>
+                <template v-else>
+                    <m-layout-grid-inner>
+                        <m-layout-grid-cell :span="8">
+                            <p @click="editOptions=true">Take a note...</p>
+                        </m-layout-grid-cell>
+                        <m-layout-grid-cell :span="4" class="mdc-layout-grid--align-right">
+                            <m-icon-button icon="check_box" title="Check list">
+                            </m-icon-button>
+                            <m-icon-button icon="brush" title="Add Sketch">
+                            </m-icon-button>
+                            <m-icon-button icon="photo" title="Add Photo">
+                            </m-icon-button>
+                        </m-layout-grid-cell>
+                    </m-layout-grid-inner>
+                </template>
             </m-layout-grid>
-     </m-card>
+        </m-card-primary-action>
+        <template v-if="editOptions"> 
+             <template slot="actionButtons">
+                <m-icon-button icon="notification_important" title="Add reminder">
+                </m-icon-button>
+                <m-icon-button icon="person_add_alt_1" title="Add user">
+                </m-icon-button>
+                <m-icon-button icon="palette" title="Colour palette">
+                </m-icon-button>
+                <m-icon-button icon="image" title="Add images">
+                </m-icon-button>
+                <m-icon-button icon="archive" title="Add to archive">
+                </m-icon-button>
+                <m-icon-button icon="more_horiz" title="Options">
+                </m-icon-button>
+             </template>
+            <template slot="actionIcons">
+                <m-button @click="editOptions=false">Close</m-button>
+            </template> 
+        </template> 
+    </m-card>
 </template>
 
 <script>
 export default {
     data() {
         return {
+            title: '',
+            task: '',
             editOptions : false,
 
+        }
+    },
+    watch: {
+        editOptions(value){
+            if(value){
+                this.$nextTick(() => {
+                    this.$refs.taskInput.focus();
+                });
+
+                //need to check for click outside to close the options if so.
+                return;
+            }
+            return;
         }
     }
 }
 </script>
-
-<style>
-
-</style>
+ 
